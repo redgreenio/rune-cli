e8fe017aa8d8dfb5760858693dbdd62250706eee
Ragunath Jawahar <ragunath@redgreen.io>
2021-01-22 00:28:21 +0530

    refactor: move configuration parsing logic away from `index.js` to `config.js`

--- bin/index.js
+++ bin/index.js
@@ -2,28 +2,13 @@
 const inquirer = require('inquirer');
 const config = require('../lib/config.js');
 
-const typeToListItem = ({types, disableEmoji}, type) => {
-  const {description, emoji, value} = types[type];
-  const prefix = emoji && !disableEmoji ? emoji + '  ' : '';
-
-  return {
-    name: prefix + (value + ':').padEnd(12, ' ') + description,
-    short: value + ': ' + description,
-    value
-  };
-};
-
-const choicesList = Object
-  .keys(config.types)
-  .map((type) => typeToListItem({types: config.types, disableEmoji: true}, type));
-
 inquirer
   .prompt([
     {
       type: 'list',
       name: 'type',
       message: 'Select the type of change you are committing:',
-      choices: choicesList,
+      choices: config.choicesList,
     }
   ])
   .then((answers) => {
